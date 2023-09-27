ae8ab7c70deced9f0461be5bc254580834e6b714
Mikaela-D <G00401392@atu.ie>
2023-09-27 15:43:42 +0100

    Created new method calculate

--- src/main/java/ie/atu/lab_1_part_2_demystifying_request_handling_springboot/CalculatorController.java
+++ src/main/java/ie/atu/lab_1_part_2_demystifying_request_handling_springboot/CalculatorController.java
@@ -1,4 +1,14 @@
 package ie.atu.lab_1_part_2_demystifying_request_handling_springboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CalculatorController {
+
+    @GetMapping("/calculate")
+    public Integer calculate(@RequestParam int a, @RequestParam  int b, @RequestParam int result) {
+
+    }
 }
