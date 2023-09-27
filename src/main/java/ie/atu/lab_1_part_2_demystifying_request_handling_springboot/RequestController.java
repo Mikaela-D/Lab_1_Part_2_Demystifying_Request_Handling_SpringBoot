87790ae2ceaa4863fdd6428beffe46e55cd57422
Mikaela-D <G00401392@atu.ie>
2023-09-27 14:55:21 +0100

    Added new code for @GetMapping Exercise 1

--- src/main/java/ie/atu/lab_1_part_2_demystifying_request_handling_springboot/RequestController.java
+++ src/main/java/ie/atu/lab_1_part_2_demystifying_request_handling_springboot/RequestController.java
@@ -1,4 +1,12 @@
 package ie.atu.lab_1_part_2_demystifying_request_handling_springboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RequestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
 }
