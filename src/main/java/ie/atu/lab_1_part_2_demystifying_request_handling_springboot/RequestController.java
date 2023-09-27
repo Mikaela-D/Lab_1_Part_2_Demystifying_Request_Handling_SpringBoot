fe739757b7721802f023133d2565ce43193d0391
Mikaela-D <G00401392@atu.ie>
2023-09-27 15:08:29 +0100

    Added new code for @PathVariable Exercise 2

--- src/main/java/ie/atu/lab_1_part_2_demystifying_request_handling_springboot/RequestController.java
+++ src/main/java/ie/atu/lab_1_part_2_demystifying_request_handling_springboot/RequestController.java
@@ -1,6 +1,7 @@
 package ie.atu.lab_1_part_2_demystifying_request_handling_springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +10,10 @@ public class RequestController {
     public String hello() {
         return "Hello, World!";
     }
+
+    @GetMapping("/greet/{name}")
+    public String greet(@PathVariable String name) {
+        return "Greetings " + name;
+    }
 }
+
