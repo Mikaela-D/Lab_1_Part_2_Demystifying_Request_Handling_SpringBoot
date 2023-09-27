473b2ddb3577f4f7ac2ce5a58bcc4a158b895056
Mikaela-D <G00401392@atu.ie>
2023-09-27 14:51:00 +0100

    Created new spring boot project

---
+++ src/main/java/ie/atu/lab_1_part_2_demystifying_request_handling_springboot/Lab1Part2DemystifyingRequestHandlingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab_1_part_2_demystifying_request_handling_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Part2DemystifyingRequestHandlingSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1Part2DemystifyingRequestHandlingSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab_1_part_2_demystifying_request_handling_springboot/Lab1Part2DemystifyingRequestHandlingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab_1_part_2_demystifying_request_handling_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1Part2DemystifyingRequestHandlingSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
